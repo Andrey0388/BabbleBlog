cec85314f7451890188a4c0a9c903bc2b70a6b1b
Andrey <kamendov.andrey@icloud.com>
2024-04-08 18:32:01 +0300

    Сайт

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,18 +1,40 @@
 function search() {
-    var searchText = document.getElementById('searchInput').value.toLowerCase();
+    var searchText = document.getElementById('searchInput').value;
     var elements = document.querySelectorAll('*');
 
+    // Удаляем предыдущую подсветку, если она есть
+    removeHighlight();
+
     elements.forEach(function(element) {
-        var originalHTML = element.innerHTML; // Сохраняем исходное содержимое элемента
-        var innerHTML = originalHTML.toLowerCase();
-        var index = innerHTML.indexOf(searchText);
-        while (index !== -1) {
-            var leftPart = originalHTML.substring(0, index);
-            var highlightedPart = originalHTML.substring(index, index + searchText.length);
-            var rightPart = originalHTML.substring(index + searchText.length);
-            element.innerHTML = leftPart + '<span class="highlighted">' + highlightedPart + '</span>' + rightPart;
-            innerHTML = rightPart.toLowerCase();
-            index = innerHTML.indexOf(searchText);
-        }
+        highlightTextInElement(element, searchText);
     });
+}
+
+function highlightTextInElement(element, searchText) {
+    if (element.tagName !== 'SCRIPT' && element.tagName !== 'STYLE') {
+        element.childNodes.forEach(function(node) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                var regex = new RegExp('(' + escapeRegExp(searchText) + ')', 'gi');
+                var newText = node.textContent.replace(regex, '<span class="highlighted">$1</span>');
+                var tempElement = document.createElement('div');
+                tempElement.innerHTML = newText;
+                while (tempElement.firstChild) {
+                    node.parentNode.insertBefore(tempElement.firstChild, node);
+                }
+                node.parentNode.removeChild(node);
+            }
+        });
+    }
+}
+
+function removeHighlight() {
+    var highlightedElements = document.querySelectorAll('.highlighted');
+    highlightedElements.forEach(function(element) {
+        element.outerHTML = element.innerHTML;
+    });
+}
+
+// Функция для экранирования специальных символов в регулярном выражении
+function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
